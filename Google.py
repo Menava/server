ae85012f10928dbc9a965185e56bf14acb02ab47
Menava <hofstadter.menava@gmail.com>
2022-12-28 19:28:21 +0630

    test

--- Google.py
+++ Google.py
@@ -12,66 +12,120 @@ temp_state=None
 google_service=None
 google_route = Blueprint('google_route', __name__)
 
-# @google_route.route('/create_service')
-# def create_service():
-#     cred = None
+@google_route.route('/create_service')
+def create_service():
+    cred = None
 
-#     pickle_file = f'token_{API_NAME}_{API_VERSION}.pickle'
-#     # print(pickle_file)
+    pickle_file = f'token_{API_NAME}_{API_VERSION}.pickle'
+    # print(pickle_file)
 
-#     if os.path.exists(pickle_file):
-#         with open(pickle_file, 'rb') as token:
-#             cred = pickle.load(token)
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
 
-#     if not cred or not cred.valid:
-#         if cred and cred.expired and cred.refresh_token:
-#             cred.refresh(Request())
-#         else:
-#             return flask.redirect('authorize')
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            return flask.redirect('authorize')
 
-#         with open(pickle_file, 'wb') as token:
-#             pickle.dump(cred, token)
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(cred, token)
 
-#     try:
-#         cred = google.oauth2.credentials.Credentials(
-#       **flask.session['credentials'])
-#         service = build(API_NAME, API_VERSION, credentials=cred)
-#         google_service=service
-#     except Exception as e:
-#         print('Unable to connect.')
-#         print(e)
+    try:
+        cred = google.oauth2.credentials.Credentials(
+      **flask.session['credentials'])
+        service = build(API_NAME, API_VERSION, credentials=cred)
+        google_service=service
+    except Exception as e:
+        print('Unable to connect.')
+        print(e)
 
-# @google_route.route('/authorize')
-# def authorize():
-#   flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-#       CLIENT_SECRET_FILE, scopes=SCOPE)
+@google_route.route('/authorize')
+def authorize():
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRET_FILE, scopes=SCOPE)
 
-#   flow.redirect_uri = flask.url_for('google_route.oauth2callback', _external=True)
+  flow.redirect_uri = flask.url_for('google_route.oauth2callback', _external=True)
 
-#   authorization_url = auth_uri = ('https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
-#                 '&client_id={}&redirect_uri={}&scope={}').format(CLIENT_ID, REDIRECT_URI, SCOPE)
-#   return flask.redirect(authorization_url)
+  authorization_url, state = flow.authorization_url(
+      # Enable offline access so that you can refresh an access token without
+      # re-prompting the user for permission. Recommended for web server apps.
+      access_type='offline',
+      # Enable incremental authorization. Recommended as a best practice.
+      include_granted_scopes='true')
+  flask.session['state'] = state
+  return flask.redirect(authorization_url)
 
 
-# @google_route.route('/oauth2callback')
-# def oauth2callback():
+@google_route.route('/oauth2callback')
+def oauth2callback():
+  state = flask.session['state']
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRET_FILE, scopes=SCOPE,state=state)
 
-#   flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-#       CLIENT_SECRET_FILE, scopes=SCOPE)
+  flow.redirect_uri = flask.url_for('google_route.oauth2callback', _external=True)
 
-#   flow.redirect_uri = flask.url_for('google_route.oauth2callback', _external=True)
+  authorization_response = flask.request.url
+  flow.fetch_token(authorization_response=authorization_response)
 
-#   authorization_response = flask.request.url
-#   flow.fetch_token(authorization_response=authorization_response)
+  credentials = flow.credentials
+  flask.session['credentials'] = credentials_to_dict(credentials)
+  return flask.redirect(flask.url_for('google_route.create_service'))
 
-#   credentials = flow.credentials
-#   flask.session['credentials'] = credentials_to_dict(credentials)
-#   return flask.redirect(flask.url_for('google_route.create_service'))
 
-# def credentials_to_dict(credentials):
-#   return {'token': credentials.token,
-#           'refresh_token': credentials.refresh_token,
-#           'token_uri': credentials.token_uri,
-#           'client_id': credentials.client_id,
-#           'client_secret': credentials.client_secret,
-#           'scopes': credentials.scopes}
+@google_route.route('/revoke')
+def revoke():
+  if 'credentials' not in flask.session:
+    return ('You need to <a href="/authorize">authorize</a> before ' +
+            'testing the code to revoke credentials.')
+
+  credentials = google.oauth2.credentials.Credentials(
+    **flask.session['credentials'])
+
+  revoke = requests.post('https://oauth2.googleapis.com/revoke',
+      params={'token': credentials.token},
+      headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+  status_code = getattr(revoke, 'status_code')
+  if status_code == 200:
+    return('Credentials successfully revoked.' + print_index_table())
+  else:
+    return('An error occurred.' + print_index_table())
+
+
+@google_route.route('/clear')
+def clear_credentials():
+  if 'credentials' in flask.session:
+    del flask.session['credentials']
+  return ('Credentials have been cleared.<br><br>' +
+          print_index_table())
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
+
+def print_index_table():
+  return ('<table>' +
+          '<tr><td><a href="/test">Test an API request</a></td>' +
+          '<td>Submit an API request and see a formatted JSON response. ' +
+          '    Go through the authorization flow if there are no stored ' +
+          '    credentials for the user.</td></tr>' +
+          '<tr><td><a href="/authorize">Test the auth flow directly</a></td>' +
+          '<td>Go directly to the authorization flow. If there are stored ' +
+          '    credentials, you still might not be prompted to reauthorize ' +
+          '    the application.</td></tr>' +
+          '<tr><td><a href="/revoke">Revoke current credentials</a></td>' +
+          '<td>Revoke the access token associated with the current user ' +
+          '    session. After revoking credentials, if you go to the test ' +
+          '    page, you should see an <code>invalid_grant</code> error.' +
+          '</td></tr>' +
+          '<tr><td><a href="/clear">Clear Flask session credentials</a></td>' +
+          '<td>Clear the access token currently stored in the user session. ' +
+          '    After clearing the token, if you <a href="/test">test the ' +
+          '    API request</a> again, you should go back to the auth flow.' +
+          '</td></tr></table>')
