11ede9ab38106a48dcff242a372524351f91b2b3
Menava <hofstadter.menava@gmail.com>
2022-12-28 23:02:32 +0630

    test

--- Google.py
+++ Google.py
@@ -27,7 +27,7 @@ def create_service():
     try:
         service = build(API_NAME, API_VERSION, credentials=cred)
         flask.session['google_service']=service
-
+        
         return jsonify("Success")
     except Exception as e:
         print('Unable to connect.')
--- routes/googleService_route.py
+++ routes/googleService_route.py
@@ -10,19 +10,15 @@ googleService_route = Blueprint('googleService_route', __name__)
 
 
 folder_id='1b0z1BZrVuP2N-47_LD9uj_A37b-aCNOe'
-google_service=session['google_service']
 
 @googleService_route.route('/drive/createfolder/<value>')
 def create_folder(value):   
-    print('test')
-    print(google_service)
-    print(value)
     file_metadata={
         'name':value,
         'mimeType':'application/vnd.google-apps.folder',
         'parents':[]
     }
-    google_service.files().create(body=file_metadata).execute()
+    session['google_service'].files().create(body=file_metadata).execute()
 
     return 'Test'
 
@@ -40,7 +36,7 @@ def add_image():
     }
     media=MediaFileUpload(os.path.join(imagePath,image.filename),mimetype='image/jpeg')
 
-    file_id=google_service.files().create(
+    file_id=session['google_service'].files().create(
         body=file_metadata,
         media_body=media,
         fields='id'
@@ -55,7 +51,7 @@ def insert_ToDrive(file_name,imagePath,folder_id):
     }
     media=MediaFileUpload(os.path.join(imagePath,file_name),mimetype='image/jpeg')
 
-    file_id=google_service.files().create(
+    file_id=session['google_service'].files().create(
         body=file_metadata,
         media_body=media,
         fields='id'
@@ -64,7 +60,7 @@ def insert_ToDrive(file_name,imagePath,folder_id):
     return file_id["id"]
 
 def delete_fileDrive(file_id):
-    google_service.files().delete(fileId=file_id).execute()
+    session['google_service'].files().delete(fileId=file_id).execute()
 
 @googleService_route.route('/drive/clearfolder')
 def clear_images():   
