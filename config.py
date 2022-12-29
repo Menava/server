60c75397033d5e120e6421edbaca7585cb89e1ed
Menava <hofstadter.menava@gmail.com>
2022-12-29 14:52:23 +0630

    test

--- config.py
+++ config.py
@@ -14,23 +14,23 @@ CLIENT_SECRET_FILE=r'/home/genshinimpact1234/mysite/server/token.json'
 API_NAME='drive'
 API_VERSION='v3'
 
-# service_folder="14goVz8uN-1xFF0SXU3hXfma5JP-ChpWg"
-# item_folder="1lJOhun8DjYi1MG1YbVaZwBQ_BoKhWowU"
-# init_customerfolder="1YXImIQ6lHuvh4zz_T5efUuTEa3OiDT82"
-# init_employeefolder="1SXn50CvDGEIKR588SF8E1UJjCd2oruOX"
-# init_imageFolder="1m2WWFI8YOSqBuWUiBqTD4BFyh96dvFSH"
-# final_customerfolder="159GJ0N1Tgzh4_0ww16w_CDWUCkyeX7ni"
-# final_employeefolder="1xiz5KJQLAZ8IAGGktse3YpuNJG3t94vN"
-# final_imageFolder="151c1-LG6q9hkpoybJwzxUymYrzjCe_YS"
+service_folder="14goVz8uN-1xFF0SXU3hXfma5JP-ChpWg"
+item_folder="1lJOhun8DjYi1MG1YbVaZwBQ_BoKhWowU"
+init_customerfolder="1YXImIQ6lHuvh4zz_T5efUuTEa3OiDT82"
+init_employeefolder="1SXn50CvDGEIKR588SF8E1UJjCd2oruOX"
+init_imageFolder="1m2WWFI8YOSqBuWUiBqTD4BFyh96dvFSH"
+final_customerfolder="159GJ0N1Tgzh4_0ww16w_CDWUCkyeX7ni"
+final_employeefolder="1xiz5KJQLAZ8IAGGktse3YpuNJG3t94vN"
+final_imageFolder="151c1-LG6q9hkpoybJwzxUymYrzjCe_YS"
 
-service_folder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
-item_folder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
-init_customerfolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
-init_employeefolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
-init_imageFolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
-final_customerfolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
-final_employeefolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
-final_imageFolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# service_folder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# item_folder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# init_customerfolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# init_employeefolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# init_imageFolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# final_customerfolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# final_employeefolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
+# final_imageFolder="1WAGNXHGxHZd5Jgb7pbt2LVb27qhod8dw"
 
 
 
