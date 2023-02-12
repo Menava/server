88e43596259c74db7ccc666bc5a462d472d76a57
Menava <minkhant.aung@my.jcu.edu.au>
2023-02-12 12:35:20 +0630

    test

--- models/voucher_outsource.py
+++ models/voucher_outsource.py
@@ -12,13 +12,14 @@ class Vouchers_outsources(db.Model):
     status=db.Column(db.Boolean,default=False)
     vouchers=db.relationship('Vouchers',backref='vouchers_outsources')
     
-    def __init__(self,voucher_id,item_name,source_name,quantity,price,total):
+    def __init__(self,voucher_id,item_name,source_name,quantity,price,total,status=False):
         self.voucher_id=voucher_id
         self.item_name=item_name
         self.source_name=source_name
         self.quantity=quantity
         self.price=price
         self.total=total
+        self.status=status
 
 class VoucherOutsource_schema(ma.Schema):
     class Meta:
