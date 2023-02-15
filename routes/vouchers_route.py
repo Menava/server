f980f34b8d877e41a0cc097bbf9962e621aa6fe9
Menava <minkhant.aung@my.jcu.edu.au>
2023-02-15 21:40:43 +0630

    test

--- routes/vouchers_route.py
+++ routes/vouchers_route.py
@@ -251,6 +251,7 @@ def getItemQty(id,option):
 		result_count=db.session.query(Items_Purchase).filter(Items_Purchase.item_id==id).order_by(Items_Purchase.id.desc()).count()
 	
 	results=db.session.query(Items_Purchase).filter(Items_Purchase.item_id==id).order_by(Items_Purchase.id.asc()).limit(result_count+1).all()
+	print('query',itemPurchases_schema.dump(results))
 	print('result 0 qty',results[0].refund_quantity)
 	print('result 0 ',results[0])
 	prev_qty=results[0].refund_quantity
