e03bcd4aa3a67924ba112e573e70dc7a145ba061
Menava <minkhant.aung@my.jcu.edu.au>
2023-02-15 21:53:51 +0630

    test

--- routes/vouchers_route.py
+++ routes/vouchers_route.py
@@ -245,7 +245,7 @@ def getItemQty(id,option):
 	if(option=='today'):
 		result_count=db.session.query(Items_Purchase).filter(Items_Purchase.item_id==id).filter(Items_Purchase.purchase_date==getTodayDate()).count()
 		print('result_count',result_count)
-		results=db.session.query(Items_Purchase).filter(Items_Purchase.item_id==id).filter(Items_Purchase.purchase_date==getTodayDate()).order_by(Items_Purchase.id.asc()).limit(result_count+1).all()
+		results=db.session.query(Items_Purchase).filter(Items_Purchase.item_id==id,Items_Purchase.purchase_date==getTodayDate()).order_by(Items_Purchase.id.asc()).limit(result_count+1).all()
 	if(option=='week'):
 		result_count=db.session.query(Items_Purchase).filter(Items_Purchase.item_id==id).filter(Items_Purchase.purchase_date>getTodayDate() - getTimeWindow('week')).order_by(Items_Purchase.id.desc()).count()
 		results=db.session.query(Items_Purchase).filter(Items_Purchase.item_id==id,Items_Purchase.purchase_date>getTodayDate() - getTimeWindow('week')).order_by(Items_Purchase.id.asc()).limit(result_count+1).all()
@@ -296,7 +296,7 @@ def getItemQty(id,option):
 	# 		pass
 	# 	itm_qty=result[0].quantity_received-result[1].refund_quantity
 
-	return itm_qty
+	return total_qty
 	
 @voucher_route.route('/itemprofit/<option>', methods=['GET'])
 def get_itemprofit(option):
