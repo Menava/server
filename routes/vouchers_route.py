7c0f30f1ef6117364f1e969fd1c44626537316da
Menava <minkhant.aung@my.jcu.edu.au>
2023-02-15 22:33:20 +0630

    test

--- routes/vouchers_route.py
+++ routes/vouchers_route.py
@@ -263,7 +263,10 @@ def getItemQty(id,option):
 		total_price+=result_price
 		prev_qty=result.refund_quantity
 
+	print('result_count',result_count)
+	print('result_allcount',result_allcount)
 	if(result_count==result_allcount):
+		print('count in')
 		total_qty+=results[0].quantity_received
 		total_price+=results[0].quantity_received*results[0].unit_price
 
